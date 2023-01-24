5d2cb9cd59b6bac7d3f6417ba53696860572f025
brysonm96 <brysonmouser@gmail.com>
2023-01-24 16:23:33 -0700

    added date script

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+let lastModif = new Date(document.lastModified);
+
+document.getElementById("lastModified").textContent = lastModif;
+
+
+let d = new Date();
+
+let year = d.getFullYear();
+
+document.getElementById("copyrightyear").textContent = year;
