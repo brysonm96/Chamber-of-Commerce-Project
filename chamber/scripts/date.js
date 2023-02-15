82d2a1713c4ddb596e4374f2efda7990cdf3cec6
brysonm96 <brysonmouser@gmail.com>
2023-02-15 13:20:08 -0700

    display banner

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -31,6 +31,20 @@ document.querySelector("#date").textContent = fulldate;
 
 document.getElementById("copyrightyear").textContent = year;
 
-
+//Displays last date the web page was modified
 let lastModif = document.lastModified;
 document.getElementById("lastModified").textContent = lastModif;
+
+//Display event banner on Mondays and Tuesdays
+function bannerDisplay(){
+	if (dayName == "Monday"){
+		document.querySelector(".reminderBanner").style.display = "grid";}
+
+	else if (dayName == "Tuesday"){
+		document.querySelector(".reminderBanner").style.display = "grid";}	
+
+	else {
+		document.querySelector(".reminderBanner").style.display = "none";}	
+}
+
+bannerDisplay()
