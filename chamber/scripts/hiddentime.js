e761df2c33ed06d4d1cbd69de6889c3e1ab17830
brysonm96 <brysonmouser@gmail.com>
2023-03-16 14:50:41 -0600

    updated changes

---
+++ chamber/scripts/hiddentime.js
@@ -0,0 +1,5 @@
+// Get current date and time
+var currentDateTime = new Date();
+  
+// Set the value of the hidden field to the current date and time
+document.getElementById("loadtime").value = currentDateTime;
